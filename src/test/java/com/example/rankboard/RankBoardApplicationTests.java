6e182a5e853866a5a35ff044ce9a4e1eaae66afd
kiomnd2 <kiomnd2@gmail.com>
2024-05-30 22:28:05 +0900

    first commit

---
+++ src/test/java/com/example/rankboard/RankBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rankboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
